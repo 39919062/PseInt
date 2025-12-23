79cf464bfd02ce04238e09ee1f58a94f609b6734
Mauri <106289038+39919062@users.noreply.github.com>
2025-12-23 17:00:15 -0300

    IMC (sencillo).py
    Este programa sencillo en PSeInt calcula el Índice de Masa Corporal (IMC) a partir del peso y la estatura ingresados por el usuario, utilizando una operación matemática básica y mostrando el resultado en pantalla.

---
+++ IMC (sencillo).py
@@ -0,0 +1,13 @@
+Algoritmo sin_titulo
+	
+	Definir peso, estatura, indice Como Real
+	
+	Escribir ("Ingrese por favor su peso en kg: ")
+	Leer peso
+	Escribir ("Ingrese por favor su estatura en metros: ")
+	Leer estatura
+	
+	indice <- peso / (estatura * estatura)
+	
+	Escribir "Su indice de masa corporal es igual a: ", indice
+FinAlgoritmo
